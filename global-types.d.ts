542c0955bdeaead82c0923f3cb92e4a6ded754df
Tomas Caraccia <tcaraccia@g2.com>
2023-11-22 22:47:18 +0100

    feat: Add callback to set

--- global-types.d.ts
+++ global-types.d.ts
@@ -1,2 +1,7 @@
 declare type UpdateMethod = (value: any) => void;
 declare type UnsubscribeFunction = () => void;
+declare type SetCallback = (value: any) => any;
+declare type SetOptions = {
+    filter: (value: any)=>boolean;
+};
+declare type NotifySubscriberOptions = SetOptions;
