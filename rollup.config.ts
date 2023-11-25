be9520240e90b68e5c057b955bca044cac25e758
Omar Luq <84993125+omarluq@users.noreply.github.com>
2023-11-25 17:37:33 -0600

    chore: optimize build, tiny increase in bundle size, huge decrease in gzipped size

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,21 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
 import sizes from 'rollup-plugin-sizes'
 
 export default {
   input: 'src/index.ts', // Entry point to your library
   output: {
-    file: 'dist/stimulus-store.js', // Output file name and location
-    format: 'umd', // Output format (UMD, CommonJS, ES module, etc.)
+    dir: 'dist/', // Output file name and location
+    format: 'cjs', // Output format (UMD, CommonJS, ES module, etc.)
     name: 'StimulusStore' // Global variable name if using UMD format
   },
   plugins: [
     typescript(), // TypeScript support
     commonjs(), // CommonJS support
+    terser(),
     resolve(), // Node.js module resolution
     analyze(),
     sizes({ details: true })
