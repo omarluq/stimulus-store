62749ed7ede1014a5f7d2944292c569ed76c6f44
Omar Luq <84993125+omarluq@users.noreply.github.com>
2023-11-25 17:02:30 -0600

    chore: better useStore error handling

--- src/errors/useStoreErrorHandlers.ts
+++ src/errors/useStoreErrorHandlers.ts
@@ -1,10 +1,24 @@
 /**
- * Checks if the stores array is defined and not empty.
+ * Checks if the stores array is defined, not empty, and is an array.
  * @param {unknown} stores - The stores array to check.
- * @throws {Error} If the stores array is undefined or empty.
+ * @throws {Error} If the stores array is undefined, empty, or not an array.
  */
 export function checkStores(stores: unknown): void {
-  if (!stores || (Array.isArray(stores) && stores.length === 0)) {
-    throw new Error(`Error: 'useStore' was called on a controller without a 'stores' static property.`)
+  if (!stores) {
+    throw new Error(
+      `Error: 'useStore' was called on a controller without a 'stores' static property. The 'stores' property is undefined.`
+    )
+  }
+
+  if (!Array.isArray(stores)) {
+    throw new Error(
+      `Error: 'useStore' was called on a controller with a 'stores' static property that is not an array.`
+    )
+  }
+
+  if (stores.length === 0) {
+    throw new Error(
+      `Error: 'useStore' was called on a controller with an empty 'stores' static property. The 'stores' array should contain at least one store.`
+    )
   }
 }
