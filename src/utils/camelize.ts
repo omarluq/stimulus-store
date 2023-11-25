56c89be3ffe3142f43584c62a2599c828f9b2f0a
Omar Luq <84993125+omarluq@users.noreply.github.com>
2023-11-24 21:28:20 -0600

    chore: Cleanup camelize

--- src/utils/camelize.ts
+++ src/utils/camelize.ts
@@ -1,6 +1,8 @@
-export function camelize(str: string, firstCharUppercase = false) {
-  const result = str.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => 
+export function camelize(str: string, firstCharUppercase: boolean = false): string {
+  const pattern1: RegExp = /(?:^\w|[A-Z]|\b\w)/g;
+  const pattern2: RegExp = /\s+/g;
+  const result: string = str.replace(pattern1, (word, index) => 
     index === 0 && !firstCharUppercase ? word.toLowerCase() : word.toUpperCase()
-  ).replace(/\s+/g, '');
+  ).replace(pattern2, '');
   return result;
 }
