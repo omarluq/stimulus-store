317c3942d2d2ff425ff0b6de9df9c39a7391a7a3
Omar Luq <84993125+omarluq@users.noreply.github.com>
2023-11-26 19:29:58 -0600

    feat: improve build, minified size drop to 1.04kb-1.26kb

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,23 +1,39 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import analyze from 'rollup-plugin-analyzer'
 import sizes from 'rollup-plugin-sizes'
 
+const production = !process.env.ROLLUP_WATCH
+
 export default {
-  input: 'src/index.ts', // Entry point to your library
-  output: {
-    dir: 'dist/', // Output file name and location
-    format: 'cjs', // Output format (UMD, CommonJS, ES module, etc.)
-    name: 'StimulusStore' // Global variable name if using UMD format
-  },
+  input: 'src/index.ts', // Entry point
+  output: [
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs', // CommonJS format
+      sourcemap: true
+    },
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'esm', // ES Module format
+      sourcemap: true
+    }
+  ],
   plugins: [
-    typescript(), // TypeScript support
+    typescript({ tsconfig: './tsconfig.json' }), // TypeScript support
     commonjs(), // CommonJS support
-    terser(),
     resolve(), // Node.js module resolution
-    analyze(),
-    sizes({ details: true })
+    production &&
+      terser({
+        // Terser for minification in production
+        output: { comments: false },
+        compress: {
+          drop_console: true // Remove console logs in production
+        }
+      }),
+    analyze(), // Bundle analysis
+    sizes() // Size analysis
   ]
 }
