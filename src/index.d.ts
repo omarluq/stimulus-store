7d4a00856d300ef5af897920c993dd13bcc2e45f
Tomss Caraccia <tom@homedev.local>
2024-02-25 03:35:38 -0600

    chore: cleanup ts structure

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+export { CurrentValueCallback } from "./types/currentValueCallback";
+export { NotifySubscriberOptions } from "./types/notifySubscriberOptions";
+export { SetCallback } from "./types/setCallback";
+export { SetOptions } from "./types/setOptions";
+export { StoreController } from "./types/storeController";
+export { StoreOptions } from "./types/storeOptions";
+export { StoreValue } from "./types/storeValue";
+export { Subscription } from "./types/subscription";
+export { TypeKey } from "./types/typeKey";
+export { UnsubscribeFunction } from "./types/unsubscribeFunction";
+export { UpdateMethod } from "./types/updateMethod";
+
+declare module "stimulus-store" {}
