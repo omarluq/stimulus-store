4b3984ba9bf90617198a7f8876dda38afee73502
Omar Luq <84993125+omarluq@users.noreply.github.com>
2025-03-25 18:12:45 -0500

    chore: remove size-limit gha

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,50 +1,50 @@
-import typescript from '@rollup/plugin-typescript'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import terser from '@rollup/plugin-terser'
-import analyze from 'rollup-plugin-analyzer'
-import sizes from 'rollup-plugin-sizes'
-import copy from 'rollup-plugin-copy'
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import analyze from "rollup-plugin-analyzer";
+import sizes from "rollup-plugin-sizes";
+import copy from "rollup-plugin-copy";
 
-const production = !process.env.ROLLUP_WATCH
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/index.ts', // Entry point
+  input: "src/index.ts", // Entry point
   output: [
     {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs', // CommonJS format
-      sourcemap: true
+      file: "dist/bundle.cjs.js",
+      format: "cjs", // CommonJS format
+      sourcemap: true,
     },
     {
-      file: 'dist/bundle.esm.js',
-      format: 'esm', // ES Module format
-      sourcemap: true
+      file: "dist/bundle.esm.js",
+      format: "esm", // ES Module format
+      sourcemap: true,
     },
     {
-      file: 'dist/bundle.umd.js',
-      name: 'StimulusStore',
-      format: 'umd',
-      sourcemap: true
-    }
+      file: "dist/bundle.umd.js",
+      name: "StimulusStore",
+      format: "umd",
+      sourcemap: true,
+    },
   ],
   plugins: [
-    typescript({ tsconfig: './tsconfig.json' }), // TypeScript support
+    typescript({ tsconfig: "./tsconfig.json" }), // TypeScript support
     copy({
-      targets: [{ src: 'src/**/*.d.ts', dest: 'dist' }],
-      flatten: false
+      targets: [{ src: "src/**/*.d.ts", dest: "dist" }],
+      flatten: false,
     }),
     commonjs(), // CommonJS support
     resolve(), // Node.js module resolution
     production &&
-    terser({
-      // Terser for minification in production
-      output: { comments: false },
-      compress: {
-        drop_console: true // Remove console logs in production
-      }
-    }),
+      terser({
+        // Terser for minification in production
+        output: { comments: false },
+        compress: {
+          drop_console: true, // Remove console logs in production
+        },
+      }),
     analyze(), // Bundle analysis
-    sizes() // Size analysis
-  ]
-}
+    sizes({ details: true }), // Size analysis
+  ],
+};
